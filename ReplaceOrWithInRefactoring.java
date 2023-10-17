888eb1568ed4d946da735ca3de00e14655d94366
Agnelli Nicolas <nico_edlp@hotmail.es>
2023-10-17 19:18:49 -0300

    Creacion clase ReplaceOrWitInRefactoring

---
+++ ReplaceOrWithInRefactoring.java
@@ -0,0 +1,22 @@
+
+public class ReplaceOrWithInRefactoring extends Refactoring{
+
+	@Override
+	protected boolean checkPreconditions(String text) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected String transform(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean checkPostconditions(String text) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
