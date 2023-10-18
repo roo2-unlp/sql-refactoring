b92617719db813e404af386794432cec7b0c927d
Juancito <juan.voolpe@gmail.com>
2023-10-18 11:45:39 -0300

    deleting unused imports

--- CountRefactoringTest.java
+++ CountRefactoringTest.java
@@ -1,10 +1,5 @@
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.beans.Transient;
-
 import org.junit.Test;
-
 
 class CountRefactoringTest{
     
@@ -14,7 +9,6 @@ class CountRefactoringTest{
         Refactoring refactoring = new CountRefactoring();
         result = refactoring.refactor("SELECT * FROM table_name;");  
         assertFalse(result.contains("*"));
- 
     }
 
 }
