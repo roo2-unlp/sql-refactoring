a24a341e802cd64e7609e3793f8f639827e38471
Nicolas De Rose <nicoderose_mg@hotmail.com.ar>
2023-10-31 23:14:23 -0300

    agrego cambios en los test

--- LimitWithOrderByTest.java
+++ LimitWithOrderByTest.java
@@ -1,13 +1,11 @@
 import static org.junit.Assert.assertTrue;
-
 import java.beans.Transient;
-
 import org.junit.Test;
 
 public class LimitWithOrderByTest {
-
+    
     @Test 
-    public void nullrefactorAQuery() throws RefactoringException {
+    public void limitwithorderbyrefactorAQuery() throws RefactoringException {
         String result;
         Refactoring refactoring = new LimitWithOrderBy();
         result = refactoring.refactor("SELECT * FROM table_name;");  
@@ -16,19 +14,13 @@ public class LimitWithOrderByTest {
     }
 
     @Test 
-    public void nullrefactorABadQuery()  {
-        
+    public void limitwithorderbyrefactorABadQuery()  { 
         boolean failure = false;
-        Refactoring refactoring = new NullRefactoring();
+        Refactoring refactoring = new LimitWithOrderBy();
         try{
             refactoring.refactor("FOOBAR * WHERE 1=1;");  
         }
         catch(Exception e) { failure = true; }
-
         assertTrue(failure);
-            
     }
-
-
-
 }
