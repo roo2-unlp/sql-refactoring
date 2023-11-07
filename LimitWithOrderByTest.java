e94cb284d3150ed2b75d680032bfb3d612a9181b
Nicolas De Rose <nicoderose_mg@hotmail.com.ar>
2023-11-07 16:09:04 -0300

    agrego test cases

--- LimitWithOrderByTest.java
+++ LimitWithOrderByTest.java
@@ -23,4 +23,37 @@ public class LimitWithOrderByTest {
         catch(Exception e) { failure = true; }
         assertTrue(failure);
     }
+
+    // Verifica que la consulta refactorizada tenga una cláusula LIMIT pero no ORDER BY
+    @Test
+    public void limitWithOrderByValidQueryWithOrderBy() throws RefactoringException {
+        Refactoring refactoring = new LimitWithOrderBy();
+        String inputQuery = "SELECT * FROM table_name ORDER BY column_name LIMIT 5;";
+        String result = refactoring.refactor(inputQuery);
+        assertFalse(result.contains("ORDER BY"));
+        assertTrue(result.contains("LIMIT"));
+    }
+
+
+    // Verifica que la columna utilizada en ORDER BY existe en la tabla   
+    @Test
+    public void limitWithOrderByValidQueryWithOrderByColumnExists() throws RefactoringException {
+        Refactoring refactoring = new LimitWithOrderBy();
+        String inputQuery = "SELECT * FROM table_name ORDER BY existing_column LIMIT 10;";
+        String result = refactoring.refactor(inputQuery);
+        assertEquals("SELECT * FROM table_name LIMIT 10;", result);
+    }
+
+    // Verifica si existe o no columna de ordenamiento 
+    @Test
+    public void limitWithOrderByValidQueryWithOrderByNoColumn() {
+        Refactoring refactoring = new LimitWithOrderBy();
+        String inputQuery = "SELECT * FROM table_name ORDER BY LIMIT 5;"; 
+        try {
+            refactoring.refactor(inputQuery);
+            assertFalse("RefactoringException not thrown for missing ORDER BY column.", true);
+        } catch (RefactoringException e) {
+            assertTrue(true);
+        }
+    }
 }
