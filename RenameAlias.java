2b1d395892b2d87d508d0e94f2ba69abc4ec1f11
Ayelen Andereggen <ayelenandereggen@gmail.com>
2023-11-08 20:02:17 -0300

    modificaciones

--- RenameAlias.java
+++ RenameAlias.java
@@ -1,34 +1,54 @@
 
 public class RenameAlias extends Refactoring {
-
+	// alias que se quiere cambiar
 	private String alias;
-	
+	// nuevo alias
+	// private String newAlias;
+
 	public void renameAlias(String alias) {
 		this.alias = alias;
 	}
+
+	/*
+	 * public void setAlias(String alias) {
+	 * this.alias = alias;
+	 * }
+	 * 
+	 * public void setNewAlias(String newAlias) {
+	 * this.newAlias = newAlias;
+	 * }
+	 */
 	public boolean aliasExist() {
 		return true;
 	}
+
 	public void newAliasNotExist() {
-		
+
 	}
+
 	@Override
 	protected boolean checkPreconditions(String text) {
-		// TODO Auto-generated method stub
+		// Que la consulta tenga un alias
+
+		// Que no exista otro alias igual al que quiero cambiar
+		// No utilizar palabras reservadas
+		// Que el cambio no genere conflictos en la subquery
+		// Que el nuevo alias no exista
+
+		// Que el nuevo alias no sea igual al nombre de la tabla o de otra columna
 		return false;
 	}
+
 	@Override
 	protected String transform(String text) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	protected boolean checkPostconditions(String text) {
-		// TODO Auto-generated method stub
+		// Que el alias haya sido modificado en todos los lugares y no esté el anterior
 		return false;
 	}
-	
-	
-	
-	
+
 }
--- RenameAliasTest.java
+++ RenameAliasTest.java
@@ -6,42 +6,45 @@ import org.junit.jupiter.api.Test;
 
 public class RenameAliasTest {
 
-	/*@Test
-	public void renameAlias() {
-		String result;
-		Refactoring refactoring = new RenameAlias();
-		String query = "FROM Products AS Pro\r\n"
-				+ "	INNER JOIN Categories AS Cat\r\n"
-				+ "	ON Pro.CategoryID = Cat.CategoryID";
-		assertNotEqual(nombreAlias, refactoring.refactor(query))
-		refactoring.setAlias(nombreAlias);
-		assertEquals()
-	}
-	*/
+	/*
+	 * @Test
+	 * public void renameAlias() {
+	 * String result;
+	 * Refactoring refactoring = new RenameAlias();
+	 * String query = "FROM Products AS Pro\r\n"
+	 * + "	INNER JOIN Categories AS Cat\r\n"
+	 * + "	ON Pro.CategoryID = Cat.CategoryID";
+	 * assertNotEqual(nombreAlias, refactoring.refactor(query))
+	 * refactoring.setAlias(nombreAlias);
+	 * assertEquals()
+	 * }
+	 */
 	@Test
-    public void testRenameAlias() throws RefactoringException {
+	public void testRenameAlias() throws RefactoringException {
 		RenameAlias refactoring = new RenameAlias();
-		refactoring.renameAlias("pais");
-        String query = "SELECT nombre AS nom_pais FROM paises";
+		refactoring.renameAlias("pais"); // agregaría también nuevo alias
+		String query = "SELECT nombre AS nom_pais FROM paises";
 
-        String refactoredSql = refactoring.refactor(query);
+		// chequearía las precondiciones antes de refactorizar
+		String refactoredSql = refactoring.refactor(query);
 
-        boolean preconditions = refactoring.checkPreconditions(query);
-        boolean postconditions = refactoring.checkPostconditions(refactoring.refactor(query));
+		boolean preconditions = refactoring.checkPreconditions(query);
+		boolean postconditions = refactoring.checkPostconditions(refactoring.refactor(query));
 
-        // Verifica que las condiciones previas y posteriores se cumplan
-        assertTrue(preconditions);
-        assertTrue(postconditions);
+		// Verifica que las condiciones previas y posteriores se cumplan
+		assertTrue(preconditions);
+		assertTrue(postconditions);
 
-        assertEquals("SELECT nombre AS pais FROM paises", refactoredSql);
-    }
-	
+		assertEquals("SELECT nombre AS pais FROM paises", refactoredSql);
+	}
+
 	@Test
 	public void aliasExist() {
 		RenameAlias refactoring = new RenameAlias();
+		// chequear que el alias que quiero cambiar exista
 		refactoring.renameAlias("pais");
 		String query = "SELECT nombre AS pais FROM paises";
-		
+
 		try {
 			refactoring.refactor(query);
 			fail("se deberia lanzar una excepción porque el alias ya existe");
@@ -53,23 +56,24 @@ public class RenameAliasTest {
 	@Test
 	public void newAliasNotExist() {
 		RenameAlias refactoring = new RenameAlias();
+		// chequear que el nuevo alias no exista
 		refactoring.renameAlias("pais");
 		String query = "SELECT nombre AS p FROM paises";
 		String refactoredSql = refactoring.refactor(query);
-        assertEquals("SELECT nombre AS pais FROM paises", refactoredSql);
+		assertEquals("SELECT nombre AS pais FROM paises", refactoredSql);
 	}
-	
+
 	@Test
 	public void postConditionsFailed() {
 		RenameAlias refactoring = new RenameAlias();
 		refactoring.renameAlias("legajo* ");
-        String query = "SELECT numero_de_legajo AS nro_legajo FROM estudiantes";
-        try {
-        	String refactoredSql = refactoring.refactor(query);
+		String query = "SELECT numero_de_legajo AS nro_legajo FROM estudiantes";
+		try {
+			String refactoredSql = refactoring.refactor(query);
 			fail("se deberia lanzar una excepción porque el alias es válido pero"
 					+ "la consulta es sintacticamente incorrecta");
 		} catch (RefactoringException e) {
 			assertEquals("Postconditions not met.", e.getMessage());
 		}
-  	}
+	}
 }
