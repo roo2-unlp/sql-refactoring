63af394fb90ca2b7da05b50e02bf97813405db05
Ayelen Andereggen <ayelenandereggen@gmail.com>
2023-11-14 10:49:19 -0300

    Comente alias visitor

--- AliasVisitor.java
+++ AliasVisitor.java
@@ -5,13 +5,7 @@ import sqlitegrammar.*;
 public class AliasVisitor extends SQLiteParserBaseVisitor<String> {
     @Override
     public String visitColumn_alias(SQLiteParser.Column_aliasContext ctx){
-        
+        //Todavia no se bien como funciona
         ctx.column_alias().getText().equals(alias)
         return contexto;
     }
-
-    public static void main(String[] args) {
-        PruebaVisitor p = new PruebaVisitor();
-        p.visitParse(SQLiteParser.ParseContext ctx);
-    }
-}
