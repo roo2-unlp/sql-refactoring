00fd56465e8953566566996baeb0deb41bbd55f6
Santo Ojeda <santoojeda@SamsoPro.local>
2023-11-22 14:52:52 -0300

    [Partial] - clean code

--- GroupByRefactoring.java
+++ GroupByRefactoring.java
@@ -7,7 +7,7 @@ import sqlitegrammar.*;
 
 public class GroupByRefactoring extends Refactoring {
 
-    private String preconditionText = "GROUP BY";
+    private String preconditionText = " GROUP BY ";
     private String stmtParameter;
 
     
@@ -31,7 +31,9 @@ public class GroupByRefactoring extends Refactoring {
    
         //Preguntar en caso que sea un sql sintacticamente correcto pero sin group by 
         //Preguntar en caso que sea un sql sintacticamente incorrecto sin group by deberiamos tomarlo como algo que se puede transformar? ya que el getNumberOfSystaxErrors ej: Select name from persona p name; donde faltaria agregar el group by al final de p ?
-        
+        System.out.println("text en pre conditions");
+        System.out.println(text);
+
         if (parser.getNumberOfSyntaxErrors()>0) {
             return false;            
         }
@@ -53,7 +55,7 @@ public class GroupByRefactoring extends Refactoring {
         finalTransformed.append(transformedText)
         .delete(finalTransformed.indexOf(";<EOF>", 0), finalTransformed.capacity());
         
-        finalTransformed.append(preconditionText+" "+this.stmtParameter+";");
+        finalTransformed.append(preconditionText+this.stmtParameter+";");
         
         return finalTransformed.toString();
         
