684bd29af68252efadef7e8a0693bfb2193243f9
Agustin Di Nardi <agustindinardi@gmail.com>
2023-11-22 15:27:00 -0300

    transform y test
    transform y test

--- OrderByRefactoringTest.java
+++ OrderByRefactoringTest.java
@@ -6,5 +6,12 @@ import org.junit.Test;
 
 public class OrderByRefactoringTest {
 
+    @Test
+    public void testTransformAddOrderBy() throws RefactoringException{
+        Refactoring refactoring = new OrderByRefactoring();
+        String query = "SELECT P.NOMBRE FROM PERSONA P";
+        String result = refactoring.refactor(query);
+        assertEquals(result,"SELECT P.NOMBRE FROM PERSONA P ORDER BY P.NOMBRE");
+    }
 
 }
--- RefactoringOrderBy.java
+++ RefactoringOrderBy.java
@@ -26,6 +26,14 @@ public class RefactoringOrderBy extends Refactoring {
 		visitor.visit(newParseTree);
 		
 		return !visitor.isValid();
+
+	//	SQLiteParser.Ordering_termContext orderByContext = parser.ordering_term();
+    //   if (orderByContext == null) {
+    //        preconditionText = null;
+    //        return false;
+    //    }
+	//	preconditionText = newParseTree.getText();
+    //    return true;
 		
 	}
 	
@@ -33,9 +41,13 @@ public class RefactoringOrderBy extends Refactoring {
 		SQLiteParser parser = this.createSQLiteParser(text);
 	    ParseTree newParseTree = parser.parse();
 	    VisitorAddOrderBy visitorAddOrder = new VisitorAddOrderBy();
-	    
-	    
-	    
+	    visitorAddOrder.visit(newParseTree);
+
+		//visitorAddOrder.visitResult_column(text)?
+		//visitorAddOrder.visitResult_column(text)?
+
+		//String transformedQuery = text + visitorAddOrder.getTextToAppend();
+		//return transformedQuery; 
 	}
 
 	public boolean checkPostconditions(String text) {
