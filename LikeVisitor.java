ff20e56766113daf4d78919f210f5058021197aa
marcosirazabal5 <marcosirazabal5@gmail.com>
2023-11-22 17:21:12 -0300

    hay q ver porque no guarda el texto refactorizado

--- LikeVisitor.java
+++ LikeVisitor.java
@@ -3,31 +3,26 @@ import org.antlr.v4.runtime.tree.*;
 //import sqlitegrammar.*;
 
 public class LikeVisitor extends SQLiteParserBaseVisitor<String> {
-
-    @Override
-    public String visitParse(SQLiteParser.ParseContext ctx) {
-        this.visitChildren(ctx);
-        return ctx.getText();
-    }
-
     //entro al nodo q me interesa 
     public String visitExpr(SQLiteParser.ExprContext ctx) {
         //System.out.println("Entre a la expresion "+ ctx.LIKE_().getClass() + " , " + ctx.LIKE_().getText());
-
-        if ((ctx.LIKE_().getText().equalsIgnoreCase("LIKE"))){//si estamos en una expresion like, tomo el valor del hijo derecho, 
-                                                            //agregue el getText xq sino no entraba cuando se cumplia la condicion y cambie el OR por el equalsIgnoreCase
-            //this.executeAction(ctx.getChild(2));
-            // return this.executeAction(ctx.getChild(2)) ????
-            //System.out.println("entre, tipo de child: "+ ctx.getChild(2).getClass());
-            System.out.println("el hijo derecho es:"+ctx.getChild(2).getText());
-            String value = ctx.getChild(2).getText(); // tomo el texto
-            String refactoredValue = value.replace("%", ""); // le saco todos los %
-            refactoredValue = refactoredValue.replace("'", ""); // le saco todos los ''
-            refactoredValue= refactoredValue+"%"; // le agrego un unico % al final y se lo devuelvo
-            refactoredValue= "'"+refactoredValue+"'"; // le agrego los ''
-            System.out.println(refactoredValue);
+        if (ctx.LIKE_() != null){
+            if ((ctx.LIKE_().getText().equalsIgnoreCase("LIKE"))){//si estamos en una expresion like, tomo el valor del hijo derecho, 
+                                                                //agregue el getText xq sino no entraba cuando se cumplia la condicion y cambie el OR por el equalsIgnoreCase
+                //this.executeAction(ctx.getChild(2));
+                // return this.executeAction(ctx.getChild(2)) ????
+                //System.out.println("entre, tipo de child: "+ ctx.getChild(2).getClass());
+                System.out.println("el hijo derecho es:"+ctx.getChild(2).getText());
+                String value = ctx.getChild(2).getText(); // tomo el texto
+                String refactoredValue = value.replace("%", ""); // le saco todos los %
+                refactoredValue = refactoredValue.replace("'", ""); // le saco todos los ''
+                refactoredValue= refactoredValue+"%"; // le agrego un unico % al final y se lo devuelvo
+                refactoredValue= "'"+refactoredValue+"'"; // le agrego los ''
+                System.out.println(refactoredValue);
+                ctx.sta
+            }
         }
-        return visitChildren(ctx);
+        return super.visitExpr(ctx);
     }
  
     // public String executeAction(SQLiteParser.ParseTree ctx){ 
