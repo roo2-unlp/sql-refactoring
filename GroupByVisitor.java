cd6fb81438a930c083e44dcaf5fb115191cd3301
Santo Ojeda <santoojeda@SamsoPro.local>
2023-11-24 17:28:46 -0300

    [Partial] - transformed with alias & result column

--- GroupByVisitor.java
+++ GroupByVisitor.java
@@ -7,6 +7,7 @@ import sqlitegrammar.*;
 
 public class GroupByVisitor extends SQLiteParserBaseVisitor<String> {
     private StringBuilder newText = new StringBuilder();
+    private int resultCounter, columnSize = 0;
 
     @Override
     public String visitSelect_core(SQLiteParser.Select_coreContext ctx) {
@@ -14,40 +15,52 @@ public class GroupByVisitor extends SQLiteParserBaseVisitor<String> {
         if (ctx.SELECT_() != null) {
             newText.append(ctx.SELECT_().toString() + " ");
         }
+        if(!ctx.result_column().isEmpty()){
+             columnSize = ctx.result_column().size();            
+        }
+     
         // if(ctx.FROM_() != null) {
         // newText.append(ctx.FROM_().toString());
         // }
         if (ctx.groupByExpr == null) {
-            //validar que no hay groupby y agregarlo como el resto
-        } 
+            // validar que no hay groupy y agregarlo como el resto
+        }
         String result = super.visitSelect_core(ctx);
-        return result;        
+
+        return result;
 
     }
 
     @Override
     public String visitTable_name(SQLiteParser.Table_nameContext ctx) {
-        //Ver de sacar el FROM Hardcode y agregarlo con el ctx.FROM_().toString de mas arriba
-        newText.append(" FROM "+ctx.getText()+ " ");
-        return ctx.getText();
-    }
-    @Override
-    public String visitTable_alias(SQLiteParser.Table_aliasContext ctx){
-        newText.append(ctx.getText() );
-        return ctx.getText();
-    }  
-    
-    @Override
-    public String visitResult_column(SQLiteParser.Result_columnContext ctx){        
-        newText.append(ctx.getText()+",");
+        // Ver de sacar el FROM Hardcode y agregarlo con el ctx.FROM_().toString de mas
+        // arriba
+        newText.append(" FROM " + ctx.getText());
         return ctx.getText();
     }
 
+    @Override
+    public String visitTable_alias(SQLiteParser.Table_aliasContext ctx){
+       
+        newText.append(" "+ctx.getText());
+        return ctx.getText();
+    }
+
+    @Override
+    public String visitResult_column(SQLiteParser.Result_columnContext ctx) {
+        resultCounter++;
+        if (resultCounter < columnSize) {
+            newText.append(ctx.getText() + ",");
+        }else{
+            newText.append(ctx.getText());
+        }
+        
+        return ctx.getText();
+    }
 
     @Override
     public String defaultResult() {
         return this.newText.toString();
     }
-
 
 }
