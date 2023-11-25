120ab187341278efe396ac82c800adb1c06f3e99
Marchi Ariana <arianalaplaplata@hotmail.com>
2023-11-25 16:50:13 -0300

    Subo los tests corregidos y agrego los que faltaaban

--- RenameAliasTest.java
+++ RenameAliasTest.java
@@ -10,30 +10,76 @@ public class RenameAliasTest {
 	private RenameAlias refactoring;
 	private String queryWithAlias;
 	private String queryWithoutAlias;
-	private String queryRefactored;
+	private String queryColumnRefactored;
+	private String queryTableRefactored;
 
 	@Before
 	void setUp() throws Exception {
 
 		refactoring = new RenameAlias();
-		// Seteo el alias que quiero cambiar y el nuevo alias
-		refactoring.setAlias("nom_pais", "pais");
-
-		queryWithAlias = "SELECT nombre AS nom_pais FROM paises";
-		// Query sin alias
-		queryWithoutAlias = "SELECT nombre FROM paises";
-		// Query refactorizada
-		queryRefactored = "SELECT nombre AS pais FROM paises";
+		
+		//query with alias
+		queryWithAlias = "SELECT nombre_ciudad, c.nombre_pais, p.continente"
+				+ "FROM ("
+				+ "    SELECT nombre_ciudad, nombre_pais AS nom_pais"
+				+ "    FROM ciudades"
+				+ "    WHERE nom_pais IN ("
+				+ "        SELECT nombre_pais"
+				+ "        FROM paises"
+				+ "        WHERE continente = 'América'"
+				+ "    )"
+				+ ") AS cities"
+				+ "JOIN paises AS p ON c.nombre_pais = p.nombre_pais;";
+		// Query without alias
+		queryWithoutAlias = "SELECT nombre_ciudad, ciudades.nombre_pais, paises.continente"
+				+ "FROM ("
+				+ "    SELECT nombre_ciudad, nombre_pais"
+				+ "    FROM ciudades"
+				+ "    WHERE nombre_pais IN ("
+				+ "        SELECT nombre_pais"
+				+ "        FROM paises"
+				+ "        WHERE continente = 'América'"
+				+ "    )"
+				+ ") ciudades"
+				+ "JOIN paises ON ciudades.nombre_pais = paises.nombre_pais;";
+		// Query refactored
+		queryColumnRefactored = "SELECT nombre_ciudad, c.nombre_pais, p.continente"
+				+ "FROM ("
+				+ "    SELECT nombre_ciudad, nombre_pais AS pais"
+				+ "    FROM ciudades"
+				+ "    WHERE pais IN ("
+				+ "        SELECT nombre_pais"
+				+ "        FROM paises"
+				+ "        WHERE continente = 'América'"
+				+ "    )"
+				+ ") AS cities"
+				+ "JOIN paises AS p ON cities.nombre_pais = p.nombre_pais;";
+		
+		// Query refactored
+		queryTableRefactored ="SELECT nombre_ciudad, c.nombre_pais, p.continente"
+				+ "FROM ("
+				+ "    SELECT nombre_ciudad, nombre_pais AS nom_pais"
+				+ "    FROM ciudades"
+				+ "    WHERE nom_pais IN ("
+				+ "        SELECT nombre_pais"
+				+ "        FROM paises"
+				+ "        WHERE continente = 'América'"
+				+ "    )"
+				+ ") AS c"
+				+ "JOIN paises AS p ON c.nombre_pais = p.nombre_pais;";
 	}
 
 	@Test
 	public void testAliasExist() {
 		// Testea que si el alias existe, se haya cambiado correctamente
-		assertTrue(refactoring.aliasExist(queryWithAlias, "nom_pais"));
 		try {
-			assertEquals(queryRefactored, refactoring.refactor(queryWithAlias));
+			// rename alias de una columna
+			refactoring.setAlias("nom_pais", "pais");
+			assertEquals(queryColumnRefactored, refactoring.refactor(queryWithAlias));
+			//rename alias de una tabla
+			refactoring.setAlias("cities", "c");
+			assertEquals(queryTableRefactored, refactoring.refactor(queryWithAlias));
 		} catch (RefactoringException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -41,22 +87,29 @@ public class RenameAliasTest {
 	@Test
 	public void testAliasNotExist() {
 		// Testea que si el alias no existe la query no haya cambiado
-		assertFalse(refactoring.aliasExist(queryWithAlias, "nom_p"));
 		try {
+			// rename alias de una columna
+			refactoring.setAlias("nom_p", "pais");
+			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
+			//rename alias de una tabla
+			refactoring.setAlias("ciu", "c");
 			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
 		} catch (RefactoringException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	@Test
-	public void testQueryWithouAlias() {
+	public void testQueryWithoutAlias() {
 		// Testea que la query sin alias no se haya cambiado
 		try {
+			//rename alias de una columna
+			refactoring.setAlias("nom_p", "pais");
+			assertEquals(queryWithoutAlias, refactoring.refactor(queryWithoutAlias));
+			//rename alias de una tabla
+			refactoring.setAlias("ciudades", "c");
 			assertEquals(queryWithoutAlias, refactoring.refactor(queryWithoutAlias));
 		} catch (RefactoringException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -64,16 +117,29 @@ public class RenameAliasTest {
 	@Test
 	public void testNewAliasNotExist() {
 		// Testea que el nuevo alias no exista
-		assertTrue(refactoring.newAliasNotExist(queryWithAlias, "pais"));
+		try {
+			// rename alias de una columna
+			refactoring.setAlias("nom_pais", "pais");
+			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
+			//rename alias de una tabla
+			refactoring.setAlias("cities", "c");
+			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
+		} catch (RefactoringException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testNewAliasExist() {
 		// Testea que si el nuevo alias ya existe no se hacen cambios
 		try {
-			assertEquals(queryRefactored, refactoring.refactor(queryRefactored));
+			// rename alias de una columna
+			refactoring.setAlias("nom_pais", "p");
+			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
+			//rename alias de una tabla
+			refactoring.setAlias("cities", "c");
+			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
 		} catch (RefactoringException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -85,7 +151,6 @@ public class RenameAliasTest {
 		try {
 			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
 		} catch (RefactoringException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -93,11 +158,10 @@ public class RenameAliasTest {
 	@Test
 	public void testAliasIsNameOfTable() {
 		// Testea que si el alias es igual al nombre de la tabla no se hace el cambio
-		refactoring.setAlias("nom_pais", "paises");
+		refactoring.setAlias("p", "paises");
 		try {
 			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
 		} catch (RefactoringException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -105,11 +169,10 @@ public class RenameAliasTest {
 	@Test
 	public void testAliasIsNameOfColumn() {
 		// Testea que si el alias es igual al nombre de la columna no se hace el cambio
-		refactoring.setAlias("nom_pais", "nombre");
+		refactoring.setAlias("nom_pais", "nombre_pais");
 		try {
 			assertEquals(queryWithAlias, refactoring.refactor(queryWithAlias));
 		} catch (RefactoringException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
---
+++ TransformAliasVisitor.java
@@ -0,0 +1,7 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import sqlitegrammar.*;
+
+public class TransformAliasVisitor extends SQLiteParserBaseVisitor<String>{
+
+}
