540b0d177a055040569c3a8928a045aa2b8b6c16
Diego Rosales <dierosales07@gmail.com>
2023-11-28 13:39:03 -0300

    agrego mas tests

--- GroupByRefactoringTest.java
+++ GroupByRefactoringTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.beans.Transient;
@@ -7,27 +8,30 @@ import org.junit.Test;
 public class GroupByRefactoringTest {
     
     @Test 
-    public void preconditionsTest() throws RefactoringException {
+    public void thatDistinctExistsTest() throws RefactoringException {
         String result;
         Refactoring refactoring = new GroupByRefactoring();
-        //result = refactoring.refactor("SELECT products FROM table_name;");  
-        //assertTrue(refactoring.checkPreconditions("SELECT * FROM clientes WHERE cliente_id IN (SELECT distinct cliente_id FROM pedidos);;"));
-        assertTrue(refactoring.checkPreconditions("SELECT DISTINCT producto, SUM(cantidad) as total_ventas FROM ventas"));
+        /* Pregunta si no exite el Distinct en una consulta normal/
+        assertFalse(refactoring.checkPreconditions("SELECT products FROM table_name;"));
+        /** Pregunta si exite el Distinct en una subconsulta */
+        assertTrue(refactoring.checkPreconditions("SELECT * FROM clientes WHERE cliente_id IN (SELECT distinct cliente_id FROM pedidos);;"));
+
     }
     
+    @Test 
+    public void containsAggregationFunctionTest() throws RefactoringException {
+        String result;
+        Refactoring refactoring = new GroupByRefactoring();
+        /** Pregunta si tiene una funcion de agregacion */
+        assertFalse(refactoring.checkPreconditions("SELECT DISTINCT producto, SUM(cantidad) as total_ventas FROM ventas"));
+    }
 
-    // @Test 
-    // public void nullrefactorABadQuery()  {
-        
-    //     boolean failure = false;
-    //     Refactoring refactoring = new NullRefactoring();
-    //     try{
-    //         refactoring.refactor("FOOBAR * WHERE 1=1;");  
-    //     }
-    //     catch(Exception e) { failure = true; }
-
-    //     assertTrue(failure);
-            
-    // }
+    @Test 
+    public void containsGroupByTest() throws RefactoringException {
+        String result;
+        Refactoring refactoring = new GroupByRefactoring();
+        /** Pregunta si contiene GroupBy */
+        assertFalse(refactoring.checkPreconditions("SELECT DISTINCT producto, SUM(cantidad) as total_ventas FROM ventas GROUP BY producto;"));
+    }
 
 }
