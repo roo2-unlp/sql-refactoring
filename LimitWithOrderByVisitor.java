1a444c559b7de474b9d57ecea438120d5e0061b4
Nicolas De Rose <nicoderose_mg@hotmail.com.ar>
2023-12-14 10:10:23 -0300

    agregamos cambios en la clase visitor

--- LimitWithOrderByVisitor.java
+++ LimitWithOrderByVisitor.java
@@ -10,25 +10,75 @@ public class LimitWithOrderByVisitor extends SQLiteParserBaseVisitor<String> {
 
     @Override
     public String visitSelect_stmt(SQLiteParser.Select_stmtContext ctx) {
-        if (ctx.limit_stmt() != null) {
+        /*if (ctx.limit_stmt() != null) {
             // Si ya hay una cláusula LIMIT, retornamos el texto de select_core
-           return transformedText.append(ctx.select_core(0).getText() + ctx.order_by_stmt().getText() + " " + ctx.limit_stmt().getText() + ";").toString();
+           return transformedText.append(
+                ctx.select_core(0).getChild(0).getText() + " " +
+                ctx.select_core(0).getChild(1).getText() + " " +
+                ctx.select_core(0).getChild(2).getText() + " " +
+                ctx.select_core(0).getChild(3).getText() + " " +
+                ctx.order_by_stmt().getChild(0).getText() + " " +
+                ctx.order_by_stmt().getChild(1).getText() + " " +
+                ctx.order_by_stmt().getChild(2).getText() + " " +
+                ctx.limit_stmt().getChild(0).getText() + " " +
+                ctx.limit_stmt().getChild(1).getText() + " " +
+                ";"
+        ).toString();
         }
         // Si no hay una cláusula LIMIT, la agregamos
-        return transformedText.append(ctx.select_core(0).getText() + " " + ctx.order_by_stmt().getText() + " LIMIT " + limit + ";").toString();
-    }
+        return transformedText.append(
+                ctx.select_core(0).getChild(0).getText() + " " +
+                ctx.select_core(0).getChild(1).getText() + " " +
+                ctx.select_core(0).getChild(2).getText() + " " +
+                ctx.select_core(0).getChild(3).getText() + " " +
+                ctx.order_by_stmt().getChild(0).getText() + " " +
+                ctx.order_by_stmt().getChild(1).getText() + " " +
+                ctx.order_by_stmt().getChild(2).getText() + " " +
+                " " + " LIMIT " + limit + ";").toString();*/
+
+            if (ctx.limit_stmt() != null) {
+            // Si ya hay una cláusula LIMIT, retornamos el texto de select_core
+            
+            for (int i = 0; i < ctx.select_core(0).getChildCount(); i++) {
+                transformedText.append(ctx.select_core(0).getChild(i).getText()).append(" ");
+            }
+
+            for (int i = 0; i < ctx.order_by_stmt().getChildCount(); i++) {
+                transformedText.append(ctx.order_by_stmt().getChild(i).getText()).append(" ");
+            }
+
+            for (int i = 0; i < ctx.limit_stmt().getChildCount(); i++) {
+                transformedText.append(ctx.limit_stmt().getChild(i).getText()).append(" ");
+            }
+
+            transformedText.append(";");
+            return transformedText.toString();
+        }
+
+        // Si no hay una cláusula LIMIT, la agregamos
+         for (int i = 0; i < ctx.select_core(0).getChildCount(); i++) {
+                transformedText.append(ctx.select_core(0).getChild(i).getText()).append(" ");
+            }
+
+        for (int i = 0; i < ctx.order_by_stmt().getChildCount(); i++) {
+            transformedText.append(ctx.order_by_stmt().getChild(i).getText()).append(" ");
+        }
+
+        transformedText.append(" LIMIT ").append(limit).append(";");
+        return transformedText.toString();
+        }
+    
 
     @Override
     protected String defaultResult() {
         return transformedText.toString();
     }
 
-    /*
     @Override
     protected String aggregateResult(String aggregate, String nextResult) {
         return aggregate + " " + nextResult;
     }
-    */
+    
 
     public void setLimit(int l){
         this.limit = l;
